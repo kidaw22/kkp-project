9098b8c08bd475e10d3485de7ff6ec1f5feffefa
kidaw22 <rizkyadawiyah9@gmail.com>
2021-11-04 22:46:21 +0700

    merubah nama warga jadi wargas

--- application/views/master_data/v_warga.php
+++ application/views/master_data/v_warga.php
@@ -10,7 +10,7 @@
             <table class="table table-bordered" id="table_data">
                 <thead class="text-center">
                     <th>NIK</th>
-                    <th>Nama Warga</th>
+                    <th>Nama Wargas</th>
                 </thead>
                 <tbody>
 
